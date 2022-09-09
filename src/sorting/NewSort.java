dc17dfb0cdd15c644a89234ad28f566917f03582
Akshit Mathur <Akshit Mathur@10.12.10.130>
2022-09-09 16:52:35 +0530

    New Sort

--- src/sorting/NewSort.java
+++ src/sorting/NewSort.java
@@ -1,9 +1,33 @@
 package sorting;
+import java.util.Arrays;
+
 
 public class NewSort {
+	
+	
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		int arr[] = { 4, 3, 2, 1 };
+		
+		for (int i = 0; i < arr.length; i++) {
+			 
+            // Inner nested loop pointing 1 index ahead
+            for (int j = i + 1; j < arr.length; j++) {
+ 
+                // Checking element
+                int temp = 0;
+                if (arr[j] < arr[i]) {
+ 
+                    // Swapping
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+ 
+            // Printing sorted array elements
+            System.out.print(arr[i] + " ");
+        }
 
 	}
 
